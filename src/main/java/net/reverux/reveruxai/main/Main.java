b30ede7d0907d1d936455f4afd5e9b231842c4b3
Philipp <31357895+SirSentrixDEV@users.noreply.github.com>
2018-04-02 14:53:34 +0200

    Initial commit

---
+++ src/main/java/net/reverux/reveruxai/main/Main.java
@@ -0,0 +1,19 @@
+/*
+<!----------------------------------------------------------------->
+Dieses Plugin wurde von SirSentrix geschrieben.
+Alle rechte zu diesem Plugin liegen bei SirSentrix aka. Philipp
+(www.youtube.com/c/SirSentrix)
+
+Sollte dieses Plugin ohne entsprechender Berechtigung vom Besitzer
+(SirSentrix/Philipp) auf einem Server oder einen Online Platform
+erscheinen, wir dies Konsiquenzen haben!
+
+Alle Rechte liegen bei SirSentrix/Philipp!  ©2018,02.04.2018 :: 14:44
+<!------------------------------------------------------------------>
+*/
+
+package net.reverux.reveruxai.main;
+
+public class Main {
+
+}
